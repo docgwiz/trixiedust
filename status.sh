0e7ac3a66dc6204d3706e738e70f6e7a3e7a195c
docgwiz <admin@docgwiz.com>
2025-12-08 17:08:06 -0800

    enhancements and fixes to sway config and outputs and status.sh

--- status.sh
+++ status.sh
@@ -1,4 +1,5 @@
 # status bar config file for sway
+# ~/.config/sway/status.sh
 
 # get date
 #date_formatted=$(date "+%a %Y-%m-%d %H:%M")
@@ -7,12 +8,12 @@ date_formatted=$(date "+%d-%b-%Y")
 time_formatted=$(date "+%H:%M")
 
 # get video_level
-video_level="V=$(brightnessctl -m | awk -F, '{print substr($4, 0, length($4)-1)}')%"
+video_level="V$(brightnessctl -m | awk -F, '{print substr($4, 0, length($4)-1)}')%"
 
 # get mute status
 mute_status=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')
 if [ "$mute_status" = "yes" ]; then
-  audio_level="A=XX"
+  audio_level="Axx%"
 else
 # get audio_level
 # =$(pactl get-sink-volume @DEFAULT_SINK@)
@@ -21,7 +22,7 @@ else
 # followed by a percentage sign, 
 # and finally head -n 1 to get only the first match 
 # (assuming left and right channels have the same volume).
-  audio_level="A=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '\d+(?=%)' | head -n 1)%"
+  audio_level="A$(pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '\d+(?=%)' | head -n 1)%"
 fi
 
 battery_info=$(cat /sys/class/power_supply/BAT0/status)
@@ -35,4 +36,4 @@ USERNAME=$(whoami)
 
 $iconx=""
 
-echo $USERNAME "|" "Battery" $battery_info $iconx $video_level $audio_level "|" $day_formatted $date_formatted  $time_formatted" "
+echo $USERNAME "|" "Battery" $battery_info $video_level $audio_level "|" $day_formatted $date_formatted  $time_formatted" "
